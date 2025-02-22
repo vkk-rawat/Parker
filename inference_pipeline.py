485a24bae2709f1fcba9c74d826ce45467303376
vkk-rawat <rawatvivek2603@gmail.com>
2025-02-22 05:47:28 +0530

    commit 1

--- inference_pipeline.py
+++ inference_pipeline.py
@@ -1,21 +1,24 @@
+# Import the InferencePipeline object
 from inference import InferencePipeline
 import cv2
 
-# Function to process predictions
 def my_sink(result, video_frame):
-    if result.get("output_image"):
-        cv2.imshow("Inference Output", result["output_image"].numpy_image)
+    if result.get("output_image"): # Display an image from the workflow response
+        cv2.imshow("Workflow Image", result["output_image"].numpy_image)
         cv2.waitKey(1)
-    print("Predictions:", result.get("predictions", []))
+    print(result) # do something with the predictions of each frame
+    
 
-pipeline = InferencePipeline.init_with_model(
+# initialize a pipeline object
+pipeline = InferencePipeline.init_with_workflow(
     api_key="vMt0JUmSrXKPCmTa9jOO",
-    model_id="detect-count-and-visualize",  # Replace with actual model ID
-    video_reference=0,
-    max_fps=30,
+    workspace_name="shiv-vihar",
+    workflow_id="detect-count-and-visualize",
+    video_reference=r"C:\Users\Vivek Rawat\Downloads\2406465-uhd_3840_2160_24fps.mp4"
+
+, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
+    max_fps=100,
     on_prediction=my_sink
 )
-
-# Start inference
-pipeline.start()
-pipeline.join()
+pipeline.start() #start the pipeline
+pipeline.join() #wait for the pipeline thread to finish
